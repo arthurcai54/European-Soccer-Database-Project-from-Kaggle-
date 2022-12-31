db1c747460fa0cdfaadd37323c3f0cbca9486416
Arthur Cai <60411904+arthurcai54@users.noreply.github.com>
2022-12-30 16:23:12 -0800

    Add files via upload

---
+++ sql-insert-files/soccer.country.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM soccer.country
+LIMIT 0, 1000
+
+-- Date: 2022-12-30 16:17
+*/
+INSERT INTO `` (`id`,`name`) VALUES (1,'Belgium');
+INSERT INTO `` (`id`,`name`) VALUES (1729,'England');
+INSERT INTO `` (`id`,`name`) VALUES (4769,'France');
+INSERT INTO `` (`id`,`name`) VALUES (7809,'Germany');
+INSERT INTO `` (`id`,`name`) VALUES (10257,'Italy');
+INSERT INTO `` (`id`,`name`) VALUES (13274,'Netherlands');
+INSERT INTO `` (`id`,`name`) VALUES (15722,'Poland');
+INSERT INTO `` (`id`,`name`) VALUES (17642,'Portugal');
+INSERT INTO `` (`id`,`name`) VALUES (19694,'Scotland');
+INSERT INTO `` (`id`,`name`) VALUES (21518,'Spain');
+INSERT INTO `` (`id`,`name`) VALUES (24558,'Switzerland');
---
+++ sql-insert-files/soccer.league.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM soccer.league
+LIMIT 0, 1000
+
+-- Date: 2022-12-30 16:17
+*/
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (1,1,'Belgium Jupiler League');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (1729,1729,'England Premier League');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (4769,4769,'France Ligue 1');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (7809,7809,'Germany 1. Bundesliga');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (10257,10257,'Italy Serie A');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (13274,13274,'Netherlands Eredivisie');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (15722,15722,'Poland Ekstraklasa');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (17642,17642,'Portugal Liga ZON Sagres');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (19694,19694,'Scotland Premier League');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (21518,21518,'Spain LIGA BBVA');
+INSERT INTO `` (`id`,`country_id`,`name`) VALUES (24558,24558,'Switzerland Super League');
